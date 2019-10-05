bc7d88ba4d3cbf9ee98195571ace9b3bd10a7d29
Lehel Iszlai <iszlai.lehel@gmail.com>
2019-10-05 21:09:57 +0200

    add adder

---
+++ adder/src/main/java/com/faultt/adder/AdderApplication.java
@@ -0,0 +1,13 @@
+package com.faultt.adder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdderApplication.class, args);
+	}
+
+}
